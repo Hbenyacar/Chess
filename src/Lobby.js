20ed56e41eb7dad6ed9c27b98a1abecbd75f0c60
Hbenyacar <111990478+Hbenyacar@users.noreply.github.com>
2025-08-01 21:29:11 -0400

    Move and capture pieces without constraints

--- src/Lobby.js
+++ src/Lobby.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useRef } from 'react';
 import socket from './socket';
 import ChessBoard from './components/ChessBoard';
 
-
 function Lobby() {
   const [header, setHeader] = useState('');
   const [joinedGame, setJoinedGame] = useState(false);
@@ -10,7 +9,16 @@ function Lobby() {
   const [color, setColor] = useState('');
   const [username, setUser] = useState('');
   const [opponent, setOpponent] = useState('');
-
+  const [position, setPosition] = useState([[
+    'B4', 'B2', 'B3', 'B5', 'B6', 'B3', 'B2', 'B4'],
+    ['B1', 'B1', 'B1', 'B1', 'B1', 'B1', 'B1', 'B1'],
+    ['0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0'], 
+    ['W1', 'W1', 'W1', 'W1', 'W1', 'W1', 'W1', 'W1'],
+    ['W4', 'W2', 'W3', 'W5', 'W6', 'W3', 'W2', 'W4'
+  ]]);
 
 
   useEffect(() => {
@@ -48,7 +56,7 @@ function Lobby() {
       <header>{header}</header>
       <header>{username}</header>
       <header>{opponent}</header>
-      <ChessBoard color={color}/>
+      <ChessBoard color={color} position={position} setPosition={setPosition}/>
     </div>
 
   );
--- src/components/ChessBoard.js
+++ src/components/ChessBoard.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState, useSyncExternalStore } from "react";
 import './ChessBoard.css';
-
-const position = [[
-    '4', '2', '3', '5', '6', '3', '2', '4'],
-    ['1', '1', '1', '1', '1', '1', '1', '1'],
-    ['0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0'],
-    ['0', '0', '0', '0', '0', '0', '0', '0'], 
-    ['1', '1', '1', '1', '1', '1', '1', '1'],
-    ['4', '2', '3', '5', '6', '3', '2', '4'
-]];
 
 const pieces = ['', 'Pawn', 'Knight', 'Bishop', 'Rook', 'Queen', 'King'];
 
-const squareClick = (row, col) => {
-    console.log(`${row}, ${col}`);
+const squareClick = (row, col, color, position, setPosition, piece, setPiece, prevSquare, setPrevSquare) => {
+    if (piece !== '0' && piece !== '') {
+        if (color === 'black' && position[row][col].startsWith('B')) {
+            console.log('Valid Black!');
+        } else if (color === 'white' && position[row][col].startsWith('W')) {
+            console.log('Valid White!');
+        } else {
+            console.log('Invalid!');
+        }
+        const newPosition = position.map(row => [...row]);
+        if (prevSquare[0] != row || prevSquare[1] != col) {
+            newPosition[row][col] = piece;
+            newPosition[prevSquare[0]][prevSquare[1]] = '0';
+            setPosition(newPosition);
+        }
+        setPiece('0');
+    } else {
+        setPrevSquare([[row],[col]]);
+        setPiece(position[row][col]);
+    }
 }
 
-const ChessBoard = ({color}) => {
+const ChessBoard = ({color, position, setPosition}) => {
     console.log(`color ${color}`)
     const board = [];
-    const className = `chess-board${color = 'black' ? ' rotate' : ''}`;
+    const className = `chess-board${color === 'black' ? ' rotate' : ''}`;
+    const [piece, setPiece] = useState('');
+    const [prevSquare, setPrevSquare] = useState([[null],[]]);
     console.log(`${className}`)
 
     // ------------
@@ -42,8 +51,8 @@ const ChessBoard = ({color}) => {
             squares.push(
                 <div key={`${row}-${col}`} 
                 className={`square ${isBlack ? 'black' : 'white'}`}
-                onClick={() => squareClick(row, col)}>
-                    {pieces[position[row][col]]}
+                onClick={() => squareClick(row, col, color, position, setPosition, piece, setPiece, prevSquare, setPrevSquare)}>
+                    {pieces[position[row][col].substring(1,position[row][col].length)]}
                 </div>
             );
         }
