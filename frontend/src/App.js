a384bba66ec82c3a1b005b769606f856f18014c2
Hbenyacar <111990478+Hbenyacar@users.noreply.github.com>
2025-08-28 13:23:27 -0400

    Deploy to gh pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Lobby from './Lobby';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lobby" element={<Lobby />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/lobby" element={<Lobby />} />
+    </Routes>
   );
 }
 
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Create the socket once and export it
-const socket = io("http://localhost:4000", {
+const socket = io("https://chess-6pk6.onrender.com", {
   autoConnect: true, // Optional: automatically connects on import
   transports: ['websocket'], // Avoid long polling if preferred
 });
